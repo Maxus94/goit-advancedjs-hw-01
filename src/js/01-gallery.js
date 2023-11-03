0cf93ad68c73c2af9953e89932f047fb8631347a
Maxim <kmaxusk@gmail.com>
2023-11-03 11:15:20 +0200

    first

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+//console.log(SimpleLightbox);
+//console.log(galleryItems);
 
-console.log(galleryItems);
+let markUp = "";
+galleryItems.forEach(
+  ({ preview, original, description }) =>
+    (markUp += `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>
+`)
+);
+
+const ul = document.querySelector(".gallery");
+
+ul.innerHTML = markUp;
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
